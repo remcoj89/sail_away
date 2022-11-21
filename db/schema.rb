32d90e01217a3a96df666cf94827cbe1986d02cd
daniel foca <focadaniel@gmail.com>
2022-11-21 17:26:51 +0100

    Create boats migration

--- db/schema.rb
+++ db/schema.rb
@@ -14,17 +14,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_21_141447) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "boats", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.string "address"
-    t.integer "price"
-    t.bigint "users_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["users_id"], name: "index_boats_on_users_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -41,5 +30,4 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_21_141447) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "boats", "users", column: "users_id"
 end
