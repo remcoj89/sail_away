ec83caac24f02fc1ea38277fa07cb988eac88383
remcojonk <remcojonk@hotmail.com>
2022-11-22 12:02:40 +0100

    createt CRUD methods in the controller

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -4,17 +4,39 @@ class BoatsController < ApplicationController
     @boats = Boat.all
   end
 
+  def show
+    @boat = Boat.find(params[:id])
+  end
+
   def new
     @boat = Boat.new
   end
 
   def create
     @boat = Boat.new(boat_params)
-    @boat.save
-    redirect_to boats_path
+    @boat.user = current_user
+    if @boat.save
+      redirect_to boats_path(@boat)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
-  
+  def edit
+    @boat = boat.find(params[:id])
+    @boat.user = current_user
+  end
+
+  def update
+    @boat = boat.find(params[:id])
+    @boat.update(params[:boat])
+  end
+
+  def destroy
+    @boat = Boat.find(params[:id])
+    @boat.destroy
+    redirect_to boats_path(@boat.restaurant), status: :see_other
+  end
 
   private
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_21_162541) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_22_103328) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,10 +19,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_21_162541) do
     t.text "description"
     t.string "address"
     t.integer "price"
-    t.bigint "users_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["users_id"], name: "index_boats_on_users_id"
+    t.index ["user_id"], name: "index_boats_on_user_id"
   end
 
   create_table "bookings", force: :cascade do |t|
@@ -49,5 +49,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_21_162541) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "boats", "users", column: "users_id"
+  add_foreign_key "boats", "users"
 end
