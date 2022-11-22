567e16788c765115bb02499615af5e22e22d12aa
daniel foca <focadaniel@gmail.com>
2022-11-22 10:46:34 +0100

    schema git ignore

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.0].define(version: 2022_11_21_144519) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "bookings", force: :cascade do |t|
-    t.integer "start_date"
-    t.integer "end_date"
-    t.string "status"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 ActiveRecord::Schema[7.0].define(version: 2022_11_21_162541) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +23,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_21_162541) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["users_id"], name: "index_boats_on_users_id"
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.integer "start_date"
+    t.integer "end_date"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -55,5 +50,4 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_21_162541) do
   end
 
   add_foreign_key "boats", "users", column: "users_id"
-
 end
