d810d60df9bd43d5d34439222945369c3b0ce44a
remcojonk <remcojonk@hotmail.com>
2022-11-22 10:39:35 +0100

    new and create in controller

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -7,4 +7,18 @@ class BoatsController < ApplicationController
   def new
     @boat = Boat.new
   end
+
+  def create
+    @boat = Boat.new(boat_params)
+    @boat.save
+    redirect_to boats_path
+  end
+
+  
+
+  private
+
+  def boat_params
+    params.require(:boat).permit(:name, :description, :address, :price)
+  end
 end
