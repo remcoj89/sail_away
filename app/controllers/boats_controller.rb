8d77ebdda5d56b5b6c4c4fcfc3ac11518d214b97
freya heaton <freyaheaton94@gmail.com>
2022-11-23 12:44:16 +0100

    add a map

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -2,6 +2,12 @@ class BoatsController < ApplicationController
 
   def index
     @boats = Boat.all
+    @markers = @boats.geocoded.map do |boat|
+      {
+        lat: boat.latitude,
+        lng: boat.longitude
+      }
+    end
   end
 
   def show
