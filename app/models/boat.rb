f67d08ed845ef0a309d8b143f55fd1cb8c844e5e
remcojonk <remcojonk@hotmail.com>
2022-11-24 17:04:49 +0100

    bux fixed in gallery

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -3,7 +3,7 @@ class Boat < ApplicationRecord
   has_many :bookings
   has_one_attached :photo
 
-  validates :name, :description, :address, :category, :size, :capacity, :price, presence: true
+  validates :name, :description, :address, :category, :size, :capacity, :price, :photo, presence: true
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
